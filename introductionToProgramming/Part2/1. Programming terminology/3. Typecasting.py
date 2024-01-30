a3aeedb405b3fcee65fa0c05d0d753ae910495ec
abushka110 <qyjodixilu@gmail.com>
2024-01-30 22:06:58 +0200

    Part2 - Programming terminology

---
+++ introductionToProgramming/Part2/1. Programming terminology/3. Typecasting.py
@@ -0,0 +1,6 @@
+numFloat = float(input("Please type in a number: "))
+numInt = int(numFloat)
+decimalPart = numFloat - float(numInt)
+
+print(f"Integer part: {numInt}")
+print(f"Decimal part: {decimalPart}")
