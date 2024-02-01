0580cb837945ac4093e8039997915fb1c96fbe7e
abushka110 <qyjodixilu@gmail.com>
2024-02-01 17:51:01 +0200

    solution for sum and mean

---
+++ introductionToProgramming/Part1/4ArithmeticOperations/Sum-And-Mean.py
@@ -0,0 +1,10 @@
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+num3 = int(input("Number 3: "))
+num4 = int(input("Number 4: "))
+
+numbersSum = num1 + num2 + num3 + num4
+
+numberMean = float(numbersSum) / 4
+
+print(f"The sum of the numbers is {numbersSum} and the mean is {numberMean}")
