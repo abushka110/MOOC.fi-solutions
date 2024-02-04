eee9d17b870444bfc33eb7ca85b68749c2f7d98c
abushka110 <qyjodixilu@gmail.com>
2024-02-04 11:17:39 +0200

    The Second Occurence - solved

--- introductionToProgramming/Part3/2-Working-With-Strings/15-The-Second-Occurrence.py
+++ introductionToProgramming/Part3/2-Working-With-Strings/15-The-Second-Occurrence.py
@@ -1,25 +1,13 @@
 word = input("Please type in a string: ")
 character = input("Please type in a substring: ")
 
-lenOfChar = len(character)
-
 indexChar = word.find(character)
 
-secondWordIndex = 0
-
-index2 = 0
-
-while indexChar != -1 or index2 != 2:
-    secondWordIndex += indexChar
-    index2 += 1
-    if lenOfChar == 2:
-        word = word[indexChar+2:]
-        secondWordIndex += 1
-    else:
-        word = word[indexChar+1:]
-    indexChar = word.find(character)
-
-if index2 == 2:
-    print(f"The second occurrence of the substring is at index {secondWordIndex}.")
-else:
+if indexChar == -1:
     print("The substring does not occur twice in the string.")
+else:
+    indexChar = word.find(character, indexChar + len(character))
+    if indexChar == -1:
+        print("The substring does not occur twice in the string.")
+    else:
+        print(f"The second occurrence of the substring is at index {indexChar}.")
