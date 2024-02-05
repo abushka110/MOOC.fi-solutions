8ce596f5d021a48ae1391e016fa7ed6103dfa570
abushka110 <qyjodixilu@gmail.com>
2024-02-05 18:11:02 +0200

    not solved

---
+++ introductionToProgramming/Part3/4-Defining-Functions/6-Chessboard.py
@@ -0,0 +1,21 @@
+# solution 1
+def chessboard(columns):
+    rows = columns
+    binary = ""
+    index = 1
+    index2 = 1
+    while index2 <= columns:
+        # while :
+        #     if index % 2 == 0:
+        #         binary += "0"
+        #     else:
+        #         binary += "1"
+        #     index += 1
+        # print(binary)
+        # binary = ""
+        # index2 += 1
+    
+
+# Testing the function
+if __name__ == "__main__":
+    chessboard(3)
