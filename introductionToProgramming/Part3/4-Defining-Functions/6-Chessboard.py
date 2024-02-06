f5de3c66278949d69fe818879ed66404b4126b6d
abushka110 <qyjodixilu@gmail.com>
2024-02-06 23:18:16 +0200

    correct only for odd numbers

--- introductionToProgramming/Part3/4-Defining-Functions/6-Chessboard.py
+++ introductionToProgramming/Part3/4-Defining-Functions/6-Chessboard.py
@@ -1,21 +1,22 @@
-# solution 1
-def chessboard(columns):
-    rows = columns
-    binary = ""
-    index = 1
-    index2 = 1
-    while index2 <= columns:
-        # while :
-        #     if index % 2 == 0:
-        #         binary += "0"
-        #     else:
-        #         binary += "1"
-        #     index += 1
-        # print(binary)
-        # binary = ""
-        # index2 += 1
+# solution 1, correct only for odd numbers
+# def chessboard(columns):
+#     rows = columns
+#     binary = ""
+#     index = 1
+#     while rows <= columns * columns:
+#         while index <= rows:
+#             if index % 2 == 0:
+#                 binary += "0"
+#             else:
+#                 binary += "1"
+#             index += 1
+#         print(binary)
+#         binary = ""
+#         rows += columns
     
 
 # Testing the function
 if __name__ == "__main__":
-    chessboard(3)
+    chessboard(8)
+    print(" ")
+    chessboard(7)
