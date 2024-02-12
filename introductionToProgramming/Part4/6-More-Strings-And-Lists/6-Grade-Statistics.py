df7a9a296517696477e0b2d76853645e04bf9e64
abushka110 <qyjodixilu@gmail.com>
2024-02-12 20:28:43 +0200

    solved - intro to programming - Part 4 - More Strings And Lists - 6

--- introductionToProgramming/Part4/6-More-Strings-And-Lists/6-Grade-Statistics.py
+++ introductionToProgramming/Part4/6-More-Strings-And-Lists/6-Grade-Statistics.py
@@ -9,15 +9,16 @@ def askPoint():
 
     # keep asking untill user input is nothing (press "Enter")
     while exam_Exercices != "":
-        exam_Points.append(int(exam_Exercices[:2]))
-        exercices_Points.append(int(exam_Exercices[3:]))
+        space_index = exam_Exercices.find(" ")
+        exam_Points.append(int(exam_Exercices[:space_index]))
+        exercices_Points.append(int(exam_Exercices[space_index + 1:]))
         exam_Exercices = input("Exam points and exercices completed: ")
 
     total_points = points_total(exam_Points, exercices_Points)
     grades_list = points_to_grades(total_points)
     print("Statistics:")
-    print(f"Points average: {points_average(total_points)}")
-    print(f"Pass percentage: {pass_percentage(grades_list)}")
+    print(f"Points average: {points_average(exam_Points, exercices_Points):.1f}")
+    print(f"Pass percentage: {pass_percentage(grades_list):.1f}")
 
     print("Grade distribution: ")
     for i in reversed(range(6)):
@@ -59,8 +60,24 @@ def points_to_grades(exam_plus_granted: list):
             grades.append(0)  
     return grades
 
-def points_average(grades):
-    return sum(grades) / len(grades) 
+# def points_average(grades):
+#     return sum(grades) / len(grades) 
+
+# 
+def points_average(exam_points: list, exercices_points: list):
+    # convert exercices points to granted points
+    granted_points = []
+    for item in exercices_points:
+        item = item / 10
+        granted_points.append(int(item))
+
+    # add exam points to granted points
+    exam_plus_granted = []
+    for item1, item2 in zip(exam_points, granted_points):
+        exam_plus_granted.append(item1 + item2)
+    
+    return sum(exam_plus_granted) / len(exam_plus_granted) 
+# 
 
 def pass_percentage(grades: list):
     temp = len(grades) - grades.count(0)
