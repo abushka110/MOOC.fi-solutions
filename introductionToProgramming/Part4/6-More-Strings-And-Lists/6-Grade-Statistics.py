85a889895c8432206ac762aa6693ff1be0ab733b
abushka110 <qyjodixilu@gmail.com>
2024-02-12 20:13:24 +0200

    not solved - 43%

--- introductionToProgramming/Part4/6-More-Strings-And-Lists/6-Grade-Statistics.py
+++ introductionToProgramming/Part4/6-More-Strings-And-Lists/6-Grade-Statistics.py
@@ -1,67 +1,70 @@
 # solution
-# function that ask points and exercises
-# points and exercises are added to lists
-# stop when user input is nothing (press "Enter")
-# then print out statistics
+# function that ask points and exercices
 def askPoint():
-    examExercisesInput = input("Exam points and exercises completed: ")
+    exam_Exercices = input("Exam points and exercices completed: ")
+    
+    # lists for points
+    exam_Points = []
+    exercices_Points = []
 
-    examExercisesList = []
-    while examExercisesInput != "":
-        examExercisesList.append(examExercisesInput)
-        examExercisesInput = input("Exam points and exercises completed: ")
+    # keep asking untill user input is nothing (press "Enter")
+    while exam_Exercices != "":
+        exam_Points.append(int(exam_Exercices[:2]))
+        exercices_Points.append(int(exam_Exercices[3:]))
+        exam_Exercices = input("Exam points and exercices completed: ")
 
-    pointsList = []
-    exercisesList = []
-    for pointsExercises in examExercisesList:
-        pointsList.append(int(pointsExercises[:2]))
-        exercisesList.append(int(pointsExercises[3:]))
-
+    total_points = points_total(exam_Points, exercices_Points)
+    grades_list = points_to_grades(total_points)
     print("Statistics:")
+    print(f"Points average: {points_average(total_points)}")
+    print(f"Pass percentage: {pass_percentage(grades_list)}")
 
-    # function that calculate GrantedPoints
-    grantedPointsList = []
-    for item in exercisesList:
-        item = item / 10
-        grantedPointsList.append(int(item))
-    # grantedPointsList = exerciseGrantedPoints(exercisesList)
-
-    # function that calculate grade
-    # grade = exam points + exercise points
-    pointsPlusExercisesPointsList = []
-    for item1, item2 in zip(pointsList, grantedPointsList):
-        pointsPlusExercisesPointsList.append(item1 + item2)
-
-    resultList = []
-    for x in range(len(pointsPlusExercisesPointsList)):
-        if pointsPlusExercisesPointsList[x] >= 0 and pointsPlusExercisesPointsList[x] <= 14 or pointsList[x] < 10:
-            resultList.append(0)
-        elif pointsPlusExercisesPointsList[x] >= 15 and pointsPlusExercisesPointsList[x] <= 17:
-            resultList.append(1)
-        elif pointsPlusExercisesPointsList[x] >= 18 and pointsPlusExercisesPointsList[x] <= 20:
-            resultList.append(2)
-        elif pointsPlusExercisesPointsList[x] >= 21 and pointsPlusExercisesPointsList[x] <= 23:
-            resultList.append(3)
-        elif pointsPlusExercisesPointsList[x] >= 24 and pointsPlusExercisesPointsList[x] <= 27:
-            resultList.append(4)
-        elif pointsPlusExercisesPointsList[x] >= 28 and pointsPlusExercisesPointsList[x] <= 30:
-            resultList.append(5)
-    # 
-
-    # function for statistics
-    # passedGrades = []
-    # for grade in grades:
-    #     if grade >= 10:
-    #         passedGrades.append(grade)
-    temp = len(resultList) - resultList.count(0)
-    passPercentage = temp * 100 / len(resultList)
-    averagePoints = sum(pointsPlusExercisesPointsList) / len(pointsPlusExercisesPointsList)
-    print(f"Points average: {averagePoints}")
-    print(f"Pass percentage: {passPercentage}")
-    print("Grade distribution:")
+    print("Grade distribution: ")
     for i in reversed(range(6)):
-        print(f"  {i}:", "*" * resultList.count(i))
-    # stats(resultList)
+        print(f"  {i}:", "*" * grades_list.count(i))
+
+# 
+def points_total(exam_points: list, exercices_points: list):
+    # convert exercices points to granted points
+    granted_points = []
+    for item in exercices_points:
+        item = item / 10
+        granted_points.append(int(item))
+
+    # add exam points to granted points
+    exam_plus_granted = []
+    for item1, item2 in zip(exam_points, granted_points):
+        if item1 < 10:
+            exam_plus_granted.append(0)
+        else:
+            exam_plus_granted.append(item1 + item2)
+    
+    return exam_plus_granted
+
+# function to convert points to grades
+def points_to_grades(exam_plus_granted: list):
+    grades = []
+    for x in range(len(exam_plus_granted)):
+        if exam_plus_granted[x] >= 28:
+            grades.append(5)
+        elif exam_plus_granted[x] >= 24:
+            grades.append(4)
+        elif exam_plus_granted[x] >= 21:
+            grades.append(3)
+        elif exam_plus_granted[x] >= 18:
+            grades.append(2)
+        elif exam_plus_granted[x] >= 15:
+            grades.append(1)
+        else:
+            grades.append(0)  
+    return grades
+
+def points_average(grades):
+    return sum(grades) / len(grades) 
+
+def pass_percentage(grades: list):
+    temp = len(grades) - grades.count(0)
+    return temp * 100 / len(grades)
 
 # test
 askPoint()
