5b20e144783d148ea6ff15c7ad03265df82fcb56
abushka110 <qyjodixilu@gmail.com>
2024-02-13 23:11:16 +0200

    solved - intro to programming - Part 4 - More Strings And Lists - 6

---
+++ introductionToProgramming/Part5/1-More-Lists/1-The-Longest-String.py
@@ -0,0 +1,14 @@
+# solution
+def longest(words: list):
+    # function returns longest item in list
+    longestWord = words[0]
+    for word in words:
+        if len(word) > len(longestWord):
+            longestWord = word
+    return longestWord
+
+
+# test
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings)) # howdydoody
