64fe0712b0e6ce2de2c9d27577fe04e1bfefbf9f
abushka110 <qyjodixilu@gmail.com>
2024-02-17 13:21:51 +0200

    solved - intro to programming - Part 5 - References- 6

--- introductionToProgramming/Part5/2-References/6-Transpose-A-Matrix.py
+++ introductionToProgramming/Part5/2-References/6-Transpose-A-Matrix.py
@@ -1,13 +1,22 @@
 # solution
+# def transpose(matrix: list):
+#     new_list = []
+#     for row in matrix:
+#         new_list.append(row[:])
+#     for row_no in range(0, len(matrix)):
+#         for column_no in range(0, len(matrix)):
+#             new_list[column_no][row_no] = matrix[row_no][column_no]
+#     for row in range(0, len(new_list)):
+#         for column in range(0, len(new_list)):
+#             matrix[row][column] = new_list[row][column]
+
+# model solution
 def transpose(matrix: list):
-    new_list = []
-    for row in matrix:
-        new_list.append(row[:])
-    for row_no in range(0, len(matrix)):
-        for column_no in range(0, len(matrix)):
-            new_list[column_no][row_no] = matrix[row_no][column_no]
-    matrix = new_list
-    return new_list
+    for i in range(0, len(matrix)):
+        for j in range(i, len(matrix)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
 
 
 # test
