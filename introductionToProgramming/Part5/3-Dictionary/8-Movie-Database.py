b4c8df8bfcf212a5a3f771e4c5e67e242e495cf2
abushka110 <qyjodixilu@gmail.com>
2024-02-21 12:15:16 +0200

    solved - intro to programming - Part 5 - Dictionary- 8,9

--- introductionToProgramming/Part5/3-Dictionary/8-Movie-Database.py
+++ introductionToProgramming/Part5/3-Dictionary/8-Movie-Database.py
@@ -1,4 +1,17 @@
 # solution
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    movie_dict = {"name": name,
+                  "director": director, 
+                  "year": year, 
+                  "runtime": runtime}
+    database.append(movie_dict)
 
 # test
 if __name__ == "__main__":
+    database = []
+    add_movie(database, "Gone with the Python", "Victor Pything", 2017, 116)
+    add_movie(database, "Pythons on a Plane", "Renny Pytholin", 2001, 94)
+    print(database)
+    # output will be:
+    # [{"name": "Gone with the Python", "director": "Victor Pything", "year": 2017, "runtime": 116},
+    #  {"name": "Pythons on a Plane", "director": "Renny Pytholin", "year": 2001, "runtime": 94}]
