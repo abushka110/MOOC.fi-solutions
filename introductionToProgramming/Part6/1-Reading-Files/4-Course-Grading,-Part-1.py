5fb6a7f0776274d6158f531f05202dfe428067a5
abushka110 <qyjodixilu@gmail.com>
2024-02-28 14:50:38 +0200

    solved - intro to programming - part 6 - reading files - 4

--- introductionToProgramming/Part6/1-Reading-Files/4-Course-Grading,-Part-1.py
+++ introductionToProgramming/Part6/1-Reading-Files/4-Course-Grading,-Part-1.py
@@ -1,5 +1,26 @@
 # solution
+student_info = input("Student information: ")
+exercise_data = input("Exercises completed: ")
+students = {}
+    
+with open(student_info) as students_file:
+    for line in students_file:
+        items = line.split(";")
+        if items[0] == "id":
+            continue
+        full_name = items[1] + " " + items[2].strip()
+        students[items[0]] = full_name
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+grades = {}
+with open(exercise_data) as exercises_file:
+    for line in exercises_file:
+        items = line.split(";")
+        if items[0] == "id":
+            continue
+        sum = 0
+        for i in range(1, len(items)):
+            sum += int(items[i])
+        grades[items[0]] = sum
+
+for id, name in students.items():
+    print(name, grades[id])
