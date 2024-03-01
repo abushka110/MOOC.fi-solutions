798fd2399c773d7855562a23df840c2398ee7efb
abushka110 <qyjodixilu@gmail.com>
2024-03-02 00:00:07 +0200

    solved - intro to programming - part 6 - reading files - 7

--- introductionToProgramming/Part6/1-Reading-Files/7-Spell-Checker.py
+++ introductionToProgramming/Part6/1-Reading-Files/7-Spell-Checker.py
@@ -1,5 +1,23 @@
 # solution
+sentence = input("Write text: ")
+sentence_list = sentence.split(" ")
+sentence_checked = ""
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+wordlist = []
+with open("wordlist.txt") as wordlist_file:
+    for word in wordlist_file:
+        word = word.replace("\n", "")
+        wordlist.append(word)
+
+
+for sentence_word in sentence_list:
+    index = 0
+    for word in wordlist:
+        if word.upper() == sentence_word.upper():
+            index = 1
+    if index == 1:
+        sentence_checked += f"{sentence_word} "
+    else:
+        sentence_checked += f"*{sentence_word}* "
+    
+print(sentence_checked)
