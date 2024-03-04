a27e3ef8ecbbb06b5ea6dd25271e7e28c2633fd5
abushka110 <qyjodixilu@gmail.com>
2024-03-04 23:08:27 +0200

    solved - intro to programming - part 6 -writing files- 2

--- introductionToProgramming/Part6/2-Writing-Files/2-Diary.py
+++ introductionToProgramming/Part6/2-Writing-Files/2-Diary.py
@@ -1,5 +1,18 @@
 # solution
-
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+print("1 - add an entry, 2 - read entries, 0 - quit")
+fun_action = int(input("Function: "))
+while fun_action != 0:
+    if fun_action == 1:
+        diary_entry = input("Diary entry: ")
+        with open("diary.txt", "a") as add_file:
+            add_file.write(f"{diary_entry}\n")
+        print("Diary saved")
+    elif fun_action == 2:
+        print("Entries:")
+        with open("diary.txt") as read_file:
+            for line in read_file:
+                line = line.replace("\n", "")
+                print(line)
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    fun_action = int(input("Function: "))
+print("Bye now!")
