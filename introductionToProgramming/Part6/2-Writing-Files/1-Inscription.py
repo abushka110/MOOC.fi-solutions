70a9a6297ab1b32086144101541d19244852d5b1
abushka110 <qyjodixilu@gmail.com>
2024-03-04 23:08:09 +0200

    solved - intro to programming - part 6 - writing files - 1

--- introductionToProgramming/Part6/2-Writing-Files/1-Inscription.py
+++ introductionToProgramming/Part6/2-Writing-Files/1-Inscription.py
@@ -1,5 +1,6 @@
 # solution
-
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+name = input("Whom should I sign this to: ")
+filename = input("Where shall I save it: ")
+ 
+with open(filename, "w") as file:
+    file.write(f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
