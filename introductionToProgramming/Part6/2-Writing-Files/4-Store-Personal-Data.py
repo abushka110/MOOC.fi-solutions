dae8a7f1df1a1c693ad4bbd0f70a08441267aa4f
abushka110 <qyjodixilu@gmail.com>
2024-03-05 13:46:34 +0200

    solved - intro to programming - part 6 -writing files- 4

--- introductionToProgramming/Part6/2-Writing-Files/4-Store-Personal-Data.py
+++ introductionToProgramming/Part6/2-Writing-Files/4-Store-Personal-Data.py
@@ -1,5 +1,8 @@
 # solution
+def store_personal_data(person: tuple):
+    with open("people.csv", "w") as file:
+        file.write(f"{person[0]};{person[1]};{person[2]}\n")
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    store_personal_data(("Paul Paulson", 37, 175.5))
