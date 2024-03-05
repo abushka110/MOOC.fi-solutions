57f88919f24921fd6e1c5e5db3ba741a8d4ab708
abushka110 <qyjodixilu@gmail.com>
2024-03-05 13:26:51 +0200

    solved - intro to programming - part 6 -writing files- 3

--- introductionToProgramming/Part6/2-Writing-Files/3-Filtering-The-Contents-Of-A-File.py
+++ introductionToProgramming/Part6/2-Writing-Files/3-Filtering-The-Contents-Of-A-File.py
@@ -1,5 +1,43 @@
 # solution
+def filter_solutions():
+    # open all files
+    with open("solutions.csv") as file, open("correct.csv", "w") as correct_file, open("incorrect.csv", "w") as incorrect_file:
+        for line in file:
+            # split into pieces
+            pieces = line.split(";")
+            calculation = pieces[1]
+            result = pieces[2]
+            # eval function calculate string expression
+            if eval(calculation) == int(result):
+                correct_file.write(line)
+            else:
+                incorrect_file.write(line)
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+# model solution
+# def filter_solutions():
+#     # Open all lines
+#     with open("solutions.csv") as source, open("correct.csv", "w") as correct_file, open("incorrect.csv", "w") as incorrect_file:
+#         for row in source:
+#             # Split into pieces
+#             pieces = row.split(";")
+ 
+#             calculation = pieces[1]
+#             result = pieces[2]
+ 
+#             # Addition or subtraction?
+#             if "+" in calculation:
+#                 operands = calculation.split("+")
+#                 # correct is True or False based on whether the calculation was correct or not
+#                 correct = int(operands[0]) + int(operands[1]) == int(result)
+#             else:
+#                 operands = calculation.split("-")
+#                 # correct is True or False based on whether the calculation was correct or not
+#                 correct = int(operands[0]) - int(operands[1]) == int(result)
+ 
+#             # Write to file
+#             if correct:
+#                 correct_file.write(row)
+#             else:
+#                 incorrect_file.write(row)
+
+filter_solutions()
