a5b63aa22c66b40963bca00352cc9f083d22def2
abushka110 <qyjodixilu@gmail.com>
2024-03-08 22:42:52 +0200

    solved - intro to programming - part 7 - Modules - 1

--- introductionToProgramming/Part7/1-Modules/1-Hypotenuse.py
+++ introductionToProgramming/Part7/1-Modules/1-Hypotenuse.py
@@ -1,5 +1,17 @@
 # solution
+from math import sqrt
+def hypotenuse(leg1: float, leg2: float):
+    leg3 = (leg1 ** 2) + (leg2 ** 2)
+    leg3 = sqrt(leg3)
+    return leg3
+
+# model solution
+# from math import sqrt
+# def hypotenuse(leg1: float, leg2: float):
+#     return sqrt((leg1 ** 2) + (leg2 ** 2))
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    print(hypotenuse(3,4)) # 5.0
+    print(hypotenuse(5,12)) # 13.0
+    print(hypotenuse(1,1)) # 1.4142135623730951
