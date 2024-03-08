6dc3d6ce46d0b2afac9d3704149065019b8b453e
abushka110 <qyjodixilu@gmail.com>
2024-03-08 15:14:16 +0200

    solved - intro to programming - part 6 - Handling Errors - 2

--- introductionToProgramming/Part6/3-Handling-Errors/2-Parameter-Validation.py
+++ introductionToProgramming/Part6/3-Handling-Errors/2-Parameter-Validation.py
@@ -1,5 +1,22 @@
 # solution
+def new_person(name: str, age: int):
+    try:
+        if name == "":
+            raise Exception("name is an empty string")
+        elif len(name.split(" ")) < 2:
+            raise Exception("name contains less than two words")
+        elif len(name) > 40:
+            raise Exception("name is longer than 40 characters")
+        elif age < 0:
+            raise Exception("age is a negative number")
+        elif age > 150:
+            raise Exception("age is greater than 150")
+        else:
+            return (name, age)
+    except ValueError:
+        raise ValueError
+
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    print(new_person('James Jameson', 160))
