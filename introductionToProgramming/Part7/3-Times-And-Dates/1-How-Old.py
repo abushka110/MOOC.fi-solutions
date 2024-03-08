c77174b04d40e288941950733dcb5b946725e0bb
abushka110 <qyjodixilu@gmail.com>
2024-03-09 00:15:37 +0200

    solved - intro to programming - part 7 - Times And Dates - 1

--- introductionToProgramming/Part7/3-Times-And-Dates/1-How-Old.py
+++ introductionToProgramming/Part7/3-Times-And-Dates/1-How-Old.py
@@ -1,5 +1,13 @@
 # solution
+from datetime import datetime
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+birth_day = int(input("Day: "))
+birth_month = int(input("Month: "))
+birth_year = int(input("Year: "))
+birth = datetime(birth_year, birth_month, birth_day)
+millennium = datetime(1999, 12, 31)
+if birth < millennium:
+    difference = millennium - birth
+    print(f"You were {difference.days} days old on the eve of the new millennium.")
+else:
+    print("You weren't born yet on the eve of the new millennium.")
