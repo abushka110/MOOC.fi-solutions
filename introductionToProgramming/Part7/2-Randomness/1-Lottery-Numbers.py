f373982eeff2ea1fbeab938771847f79da5ceb6b
abushka110 <qyjodixilu@gmail.com>
2024-03-08 23:06:27 +0200

    solved - intro to programming - part 7 - Randomness - 1

--- introductionToProgramming/Part7/2-Randomness/1-Lottery-Numbers.py
+++ introductionToProgramming/Part7/2-Randomness/1-Lottery-Numbers.py
@@ -1,5 +1,13 @@
 # solution
+from random import sample
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    numbers = []
+    number_pool = list(range(lower, upper))
+    numbers = sorted(sample(number_pool, amount))
+    return numbers
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    for number in lottery_numbers(7, 1, 40):
+        print(number)
