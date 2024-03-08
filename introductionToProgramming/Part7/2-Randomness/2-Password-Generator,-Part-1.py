e37e12f5b934567d69870a14f114d98710739827
abushka110 <qyjodixilu@gmail.com>
2024-03-08 23:45:35 +0200

    solved - intro to programming - part 7 - Randomness - 2

--- introductionToProgramming/Part7/2-Randomness/2-Password-Generator,-Part-1.py
+++ introductionToProgramming/Part7/2-Randomness/2-Password-Generator,-Part-1.py
@@ -1,5 +1,30 @@
 # solution
+# from random import shuffle
+# def generate_password(length: int):
+#     password = ""
+#     letters = 'abcdefghijklmnopqrstuvwxyz'
+#     letters_list = []
+#     for letter in letters:
+#         letters_list.append(letter)
+#     shuffle(letters_list)
+#     index = 0
+#     while len(password) < length:
+#         password += letters_list[index]
+#         index += 1
+#     return password
+
+# solution 1
+from random import choice
+from string import ascii_lowercase
+ 
+def generate_password(length: int):
+    passwd = ""
+    while len(passwd) < length:
+        passwd += choice(ascii_lowercase)
+ 
+    return passwd
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    for i in range(10):
+        print(generate_password(8))
