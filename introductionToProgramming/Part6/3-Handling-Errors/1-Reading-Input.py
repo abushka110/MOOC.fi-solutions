1d6bf00ed865177abcfbad1e13a2a9feed577de7
abushka110 <qyjodixilu@gmail.com>
2024-03-08 08:07:47 +0200

    solved - intro to programming - part 6 - Handling Errors - 1

--- introductionToProgramming/Part6/3-Handling-Errors/1-Reading-Input.py
+++ introductionToProgramming/Part6/3-Handling-Errors/1-Reading-Input.py
@@ -1,5 +1,17 @@
 # solution
+def read_input(phrase: str, num_1: int, num_2: int):
+    alert_message = f"You must type in an integer between {num_1} and {num_2}"
+    while True:
+        try:
+            number = int(input("Please type in a number: "))
+            if num_1 <= number <= num_2:
+                return number
+            else:
+                print(alert_message)
+        except ValueError:
+            print(alert_message)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
