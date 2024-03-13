a920d65d39228def8414642f7af9ac8ac70f8d34
abushka110 <qyjodixilu@gmail.com>
2024-03-13 13:17:17 +0200

    solved - intro to programming - Part 7 - Modules - 3

--- introductionToProgramming/Part7/1-Modules/3-Fractions.py
+++ introductionToProgramming/Part7/1-Modules/3-Fractions.py
@@ -1,5 +1,21 @@
 # solution
+from fractions import Fraction
+def fractionate(amount: int):
+    fraction_amount = Fraction(1, amount)
+    return [fraction_amount] * amount
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    for p in fractionate(3):
+        print(p)
+
+    print()
+
+    print(fractionate(5))
+
+    # should print out:
+    # 1/3
+    # 1/3
+    # 1/3
+
+    # [Fraction(1, 5), Fraction(1, 5), Fraction(1, 5), Fraction(1, 5), Fraction(1, 5)]
