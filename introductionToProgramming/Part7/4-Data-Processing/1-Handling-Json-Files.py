5836f84314ecf11ec7082d74cf7fa3b827d36c78
abushka110 <qyjodixilu@gmail.com>
2024-03-20 19:03:02 +0100

    solved - intro to programming - part 7 - Data Processing - 1

--- introductionToProgramming/Part7/4-Data-Processing/1-Handling-Json-Files.py
+++ introductionToProgramming/Part7/4-Data-Processing/1-Handling-Json-Files.py
@@ -1,5 +1,42 @@
-# solution
+# # solution 1
+# import json
+
+# def print_persons(filename: str):
+#     with open(filename) as students_info_file:
+#         data = students_info_file.read()
+    
+#     students = json.loads(data)
+#     for student in students:
+#         student_name = student["name"]
+#         student_age = student["age"]
+#         print(f"{student_name} {student_age} years (", end="")
+#         index = 1
+#         for hobby in student["hobbies"]:
+#             if index == len(student["hobbies"]):
+#                 print(f"{hobby}", end = "")
+#             else:
+#                 print(f"{hobby}, ", end = "")
+#                 index += 1
+#         print(")")
+
+# model solution
+import json
+
+def print_persons(filename: str):
+    with open(filename) as students_info_file:
+        data = students_info_file.read()
+    
+    students = json.loads(data)
+    for student in students:
+        student_name = student["name"]
+        student_age = student["age"]
+        student_hobbies = ", ".join(student['hobbies'])
+        print(f"{student_name} {student_age} years ({student_hobbies})")
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    print_persons("students.json")
+
+
+# Peter Pythons 27 years (coding, knitting)
+# Jean Javanese 24 years (coding, rock climbing, reading)
