b42c9a8ec681e0c93a706190f69d6c79601a27e1
abushka110 <qyjodixilu@gmail.com>
2024-04-21 16:39:31 +0200

    solved - advanced - part 8 - Objects and Methods - 2

--- advancedCourseInProgramming/Part8/1-Objects-And-Methods/2-Row-Sums.py
+++ advancedCourseInProgramming/Part8/1-Objects-And-Methods/2-Row-Sums.py
@@ -1,5 +1,20 @@
+# solution model
+def row_sums(my_matrix: list):
+    for matrix_row in my_matrix:
+        matrix_row.append(sum(matrix_row))
+        
 # solution
+def row_sums(my_matrix: list):
+    for matrix_row in my_matrix:
+        sum = 0
+        for row_element in matrix_row:
+            sum += row_element
+        matrix_row.append(sum)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    my_matrix = [[1, 2], [3, 4]]
+    row_sums(my_matrix)
+    print(my_matrix)
+    # Sample output
+    # [[1, 2, 3], [3, 4, 7]]
