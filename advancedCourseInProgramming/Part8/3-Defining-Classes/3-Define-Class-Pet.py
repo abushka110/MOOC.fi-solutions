1ea93ff01dad8c37e9f97bf420bee683a61cd717
abushka110 <qyjodixilu@gmail.com>
2024-04-21 19:30:02 +0200

    solved - advanced - part 8 - Defining Classes - 3

--- advancedCourseInProgramming/Part8/3-Defining-Classes/3-Define-Class-Pet.py
+++ advancedCourseInProgramming/Part8/3-Defining-Classes/3-Define-Class-Pet.py
@@ -1,5 +1,20 @@
 # solution
+class Pet:
+    def __init__(self, name: str, species: str, year_of_birth: int):
+        self.name = name
+        self.species = species
+        self.year_of_birth = year_of_birth
+
+def new_pet(name_new: str, species_new: str, year_of_birth_new: int):
+    return Pet(name_new, species_new, year_of_birth_new)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    fluffy = new_pet("Fluffy", "dog", 2017)
+    print(fluffy.name)
+    print(fluffy.species)
+    print(fluffy.year_of_birth)
+    # Sample output
+    # Fluffy
+    # dog
+    # 2017
