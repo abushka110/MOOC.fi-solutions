f50d004b5de8085d8790fffc8507c748d9588f61
abushka110 <qyjodixilu@gmail.com>
2024-04-21 17:01:49 +0200

    solved - advanced - part 8 - Classes and Objects - 1

--- advancedCourseInProgramming/Part8/2-Classes-And-Objects/1-List-Of-Years.py
+++ advancedCourseInProgramming/Part8/2-Classes-And-Objects/1-List-Of-Years.py
@@ -1,5 +1,19 @@
 # solution
+from datetime import date
+
+def list_years(dates: list):
+    years_sorted = []
+    for date_x in sorted(dates):
+        years_sorted.append(date_x.year)
+    return years_sorted
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    date1 = date(2019, 2, 3)
+    date2 = date(2006, 10, 10)
+    date3 = date(1993, 5, 9)
+
+    years = list_years([date1, date2, date3])
+    print(years)
+    # Sample output
+    # [1993, 2006, 2019]
