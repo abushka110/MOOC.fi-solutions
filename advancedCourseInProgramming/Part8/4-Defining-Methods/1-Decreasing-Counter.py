a9525a6662d47f08e1c27afbe2fbc1fca47512c2
abushka110 <qyjodixilu@gmail.com>
2024-04-21 20:27:57 +0200

    solved - advanced - part 8 - Defining Methods - 1 - 1

--- advancedCourseInProgramming/Part8/4-Defining-Methods/1-Decreasing-Counter.py
+++ advancedCourseInProgramming/Part8/4-Defining-Methods/1-Decreasing-Counter.py
@@ -1,5 +1,21 @@
 # solution
+class DecreasingCounter:
+    def __init__(self, initial_value: int):
+        self.value = initial_value
+
+    def print_value(self):
+        print("value:", self.value)
+
+    def decrease(self):
+        self.value -= 1
+
+    # Write the rest of the methods here!
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    counter = DecreasingCounter(10)
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
+    counter.decrease()
+    counter.print_value()
