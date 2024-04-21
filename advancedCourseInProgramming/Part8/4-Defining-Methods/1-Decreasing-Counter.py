80e6bcb4f05a2b3ac7b33346dc17e8f98bafde0e
abushka110 <qyjodixilu@gmail.com>
2024-04-22 00:38:35 +0200

    solved - advanced - part 8 - Defining Methods - 1 - 4

--- advancedCourseInProgramming/Part8/4-Defining-Methods/1-Decreasing-Counter.py
+++ advancedCourseInProgramming/Part8/4-Defining-Methods/1-Decreasing-Counter.py
@@ -2,6 +2,7 @@
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.initial_value = self.value
 
     def print_value(self):
         print("value:", self.value)
@@ -11,13 +12,21 @@ class DecreasingCounter:
             self.value -= 1
     
     def set_to_zero(self):
+        
         self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.initial_value
 
     # Write the rest of the methods here!
 
 # test
 if __name__ == "__main__":
-    counter = DecreasingCounter(100)
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
     counter.print_value()
-    counter.set_to_zero()
+    counter.reset_original_value()
     counter.print_value()
