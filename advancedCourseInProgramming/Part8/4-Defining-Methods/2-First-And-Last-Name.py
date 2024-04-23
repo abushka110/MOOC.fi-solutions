3b25ac69892e9d2a510cd9e859b8dd22140db314
abushka110 <qyjodixilu@gmail.com>
2024-04-23 17:56:14 +0200

    solved - advanced - part 8 - Defining Methods - 2

--- advancedCourseInProgramming/Part8/4-Defining-Methods/2-First-And-Last-Name.py
+++ advancedCourseInProgramming/Part8/4-Defining-Methods/2-First-And-Last-Name.py
@@ -1,5 +1,20 @@
 # solution
+class Person:
+    def __init__(self, full_name) -> None:
+        self.full_name = full_name
 
+    def return_first_name(self):
+        return self.full_name.split(" ")[0]
+
+    def return_last_name(self):
+        return self.full_name.split(" ")[1]
+    
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    peter = Person("Peter Pythons")
+    print(peter.return_first_name())
+    print(peter.return_last_name())
+
+    paula = Person("Paula Pythonnen")
+    print(paula.return_first_name())
+    print(paula.return_last_name())
