e1da2c14ffa19da9c35583d9b8b68147d08f3a76
abushka110 <qyjodixilu@gmail.com>
2024-04-24 02:52:35 +0200

    solved - advanced - part 8 - More EOC - 3

--- advancedCourseInProgramming/Part8/5-More-Examples-Of-Classes/3-Lunchcard.py
+++ advancedCourseInProgramming/Part8/5-More-Examples-Of-Classes/3-Lunchcard.py
@@ -1,5 +1,37 @@
-# solution
+# Write your solution here:
+class LunchCard:
+    def __init__(self, balance: float):
+        self.balance = balance
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+    def eat_lunch(self):
+        if self.balance > 2.6:
+            self.balance -= 2.6
+
+    def eat_special(self):
+        if self.balance > 4.6:
+            self.balance -= 4.6
+
+    def deposit_money(self, deposit):
+        if deposit > 0:
+            self.balance += deposit
+        else:
+            raise ValueError("You cannot deposit an amount of money less than zero")
+
+    def __str__(self):
+        return f"The balance is {self.balance:.1f} euros"
+    
+peters_card = LunchCard(20)
+graces_card = LunchCard(30)
+peters_card.eat_special()
+graces_card.eat_lunch()
+print(f"Peter: {peters_card}")
+print(f"Grace: {graces_card}")
+peters_card.deposit_money(20)
+graces_card.eat_special()
+print(f"Peter: {peters_card}")
+print(f"Grace: {graces_card}")
+peters_card.eat_lunch()
+peters_card.eat_lunch()
+graces_card.deposit_money(50)
+print(f"Peter: {peters_card}")
+print(f"Grace: {graces_card}")
