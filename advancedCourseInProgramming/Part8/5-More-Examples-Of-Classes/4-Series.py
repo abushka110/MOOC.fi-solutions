d1ed426cc21a77da265f77deae3848f2b03798fe
abushka110 <qyjodixilu@gmail.com>
2024-04-24 09:38:27 +0200

    solved - advanced - part 8 - More EOC - 4 - part1

--- advancedCourseInProgramming/Part8/5-More-Examples-Of-Classes/4-Series.py
+++ advancedCourseInProgramming/Part8/5-More-Examples-Of-Classes/4-Series.py
@@ -1,5 +1,15 @@
 # solution
+class Series:
+    def __init__(self, name: str, seasons_count: int, genre_list: list):
+        self.name = name
+        self.seasons_count = seasons_count
+        self.genre_list = genre_list
 
+    def __str__(self):
+        genres_string = "genres: " +", ".join(self.genre_list)
+        
+        return f"{self.name} ({self.seasons_count} seasons)\n" + genres_string + "\n" + f"no ratings"
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+    print(dexter)
