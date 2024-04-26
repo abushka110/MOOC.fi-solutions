57984c2e8e5307204d6aeb73c346627017112440
abushka110 <qyjodixilu@gmail.com>
2024-04-26 23:38:51 +0200

    solved - advanced - part 9 - OaR - 3

--- advancedCourseInProgramming/Part9/1-Objects-And-References/3-Baby-Centre.py
+++ advancedCourseInProgramming/Part9/1-Objects-And-References/3-Baby-Centre.py
@@ -1,5 +1,29 @@
-# solution
+
+# WRITE YOUR SOLUTION HERE:
+#Note! Do not change the class Person!
+
+class Person:
+    def __init__(self, name: str, age: int, height: int, weight: int):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+class BabyCentre:
+    def __init__(self):
+        self.number_of_weigh_ins = 0
+
+    def weigh(self, person: Person):
+        # return the weight of the person passed as an argument
+        return person.weight
+    
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+    print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
