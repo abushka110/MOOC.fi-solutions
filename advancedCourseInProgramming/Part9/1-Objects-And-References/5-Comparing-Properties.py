8f36de2b03d433afb9c9006c76c3df68f6ed4149
abushka110 <qyjodixilu@gmail.com>
2024-04-29 19:19:40 +0200

    solved - advanced - part 9 - OaR - 5

--- advancedCourseInProgramming/Part9/1-Objects-And-References/5-Comparing-Properties.py
+++ advancedCourseInProgramming/Part9/1-Objects-And-References/5-Comparing-Properties.py
@@ -14,7 +14,13 @@ class RealProperty:
         
     def price_difference(self, compared_to):
         return abs((self.price_per_sqm * self.square_metres) - (compared_to.price_per_sqm * compared_to.square_metres))
-        
+
+    def more_expensive(self, compared_to):
+        if self.price_per_sqm * self.square_metres > compared_to.price_per_sqm * compared_to.square_metres:
+            return True
+        else:
+            return False
+
 
 # test
 if __name__ == "__main__":
@@ -27,9 +33,17 @@ if __name__ == "__main__":
     # print(suburbs_three_bedroom.bigger(downtown_two_bedroom))
 
     # test 2
+    # central_studio = RealProperty(1, 16, 5500)
+    # downtown_two_bedroom = RealProperty(2, 38, 4200)
+    # suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    # print(central_studio.price_difference(downtown_two_bedroom))
+    # print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
+
+    # test 3
     central_studio = RealProperty(1, 16, 5500)
     downtown_two_bedroom = RealProperty(2, 38, 4200)
     suburbs_three_bedroom = RealProperty(3, 78, 2500)
 
-    print(central_studio.price_difference(downtown_two_bedroom))
-    print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))
