52b854830b7ff6850e17632b1939cf152d61ff2b
abushka110 <qyjodixilu@gmail.com>
2024-05-01 23:25:41 +0200

    solved - advanced - part 9 - OaA - 2 - 1

--- advancedCourseInProgramming/Part9/2-Objects-As-Attributes/2-A-Box-Of-Presents.py
+++ advancedCourseInProgramming/Part9/2-Objects-As-Attributes/2-A-Box-Of-Presents.py
@@ -1,5 +1,20 @@
 # solution
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self) -> str:
+        return f"Present: {self.name} ({self.weight} kg)"
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    book = Present("ABC Book", 2)
+
+    print("The name of the present:", book.name)
+    print("The weight of the present:", book.weight)
+    print("Present:", book)
+    
+    # The name of the present: ABC Book
+    # The weight of the present: 2
+    # Present: ABC Book (2 kg)
