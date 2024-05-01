afce1bca33b505367c5dc3807d1ad84735828473
abushka110 <qyjodixilu@gmail.com>
2024-05-01 23:32:10 +0200

    solved - advanced - part 9 - OaA - 2

--- advancedCourseInProgramming/Part9/2-Objects-As-Attributes/2-A-Box-Of-Presents.py
+++ advancedCourseInProgramming/Part9/2-Objects-As-Attributes/2-A-Box-Of-Presents.py
@@ -7,14 +7,40 @@ class Present:
     def __str__(self) -> str:
         return f"Present: {self.name} ({self.weight} kg)"
 
+class Box:
+    def __init__(self):
+        self.box_presents = []
+
+    def add_present(self, present: Present):
+        self.box_presents.append(present)
+
+    def total_weight(self):
+        x_weight = 0
+        for present in self.box_presents:
+            x_weight += present.weight
+        return x_weight
+
 # test
 if __name__ == "__main__":
-    book = Present("ABC Book", 2)
-
-    print("The name of the present:", book.name)
-    print("The weight of the present:", book.weight)
-    print("Present:", book)
-    
+    # test 1
+    # book = Present("ABC Book", 2)
+    # print("The name of the present:", book.name)
+    # print("The weight of the present:", book.weight)
+    # print("Present:", book)
+    # output:
     # The name of the present: ABC Book
     # The weight of the present: 2
     # Present: ABC Book (2 kg)
+
+    # test 2
+    book = Present("ABC Book", 2)
+    box = Box()
+    box.add_present(book)
+    print(box.total_weight())
+    cd = Present("Pink Floyd: Dark Side of the Moon", 1)
+    box.add_present(cd)
+    print(box.total_weight())
+    # output:
+    # 2
+    # 3
+    
