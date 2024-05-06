85e41f6164e946c268e7733a0a214fc348ea2f31
abushka110 <qyjodixilu@gmail.com>
2024-05-06 21:15:21 +0200

    solved - advanced - part 9 - Encapsulation - 1

--- advancedCourseInProgramming/Part9/3-Encapsulation/1-Car.py
+++ advancedCourseInProgramming/Part9/3-Encapsulation/1-Car.py
@@ -1,5 +1,35 @@
 # solution
+class Car:
+    def __init__(self) -> None:
+        self.__petrol_in_tank = 0
+        self.__odometer_km = 0
+    
+    def fill_up(self):
+        self.__petrol_in_tank = 60
+
+    def drive(self, km:int):
+        if self.__petrol_in_tank >= km:
+            self.__odometer_km += km
+            self.__petrol_in_tank -= km
+        else:
+            self.__odometer_km += self.__petrol_in_tank 
+            self.__petrol_in_tank -= self.__petrol_in_tank 
+    
+    def __str__(self) -> str:
+        return f"Car: odometer reading {self.__odometer_km} km, petrol remaining {self.__petrol_in_tank} litres"
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)
