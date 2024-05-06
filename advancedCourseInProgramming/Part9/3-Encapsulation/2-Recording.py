3205b799477d074d79f7baed2f0297e2680e7858
abushka110 <qyjodixilu@gmail.com>
2024-05-06 21:29:44 +0200

    solved - advanced - part 9 - Encapsulation - 2

--- advancedCourseInProgramming/Part9/3-Encapsulation/2-Recording.py
+++ advancedCourseInProgramming/Part9/3-Encapsulation/2-Recording.py
@@ -1,5 +1,27 @@
 # solution
+class Recording:
+    def __init__(self, length: int):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("Negative length is not possible")
+    
+    # A getter method
+    @property
+    def length(self):
+        return self.__length
+
+    # A setter method
+    @length.setter
+    def length(self, length_new):
+        if length_new >= 0:
+            self.__length = length_new
+        else:
+            raise ValueError("Negative length is not possible")
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    the_wall = Recording(43)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
