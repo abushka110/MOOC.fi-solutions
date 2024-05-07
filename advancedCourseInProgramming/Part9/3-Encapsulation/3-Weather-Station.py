7a07f75fc8310591cbac3bd016bef86bc8764641
abushka110 <qyjodixilu@gmail.com>
2024-05-07 18:09:01 +0200

    solved - advanced - part 9 - Encapsulation - 3

--- advancedCourseInProgramming/Part9/3-Encapsulation/3-Weather-Station.py
+++ advancedCourseInProgramming/Part9/3-Encapsulation/3-Weather-Station.py
@@ -1,5 +1,32 @@
 # solution
+class WeatherStation:
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        self.__observations = 0
+        self.__latest_observation = ""
 
+    def add_observation(self, observation: str):
+        self.__latest_observation = observation
+        self.__observations += 1
+
+    def latest_observation(self):
+        return self.__latest_observation
+
+    def number_of_observations(self):
+        return self.__observations
+    
+    def __str__(self) -> str:
+        return f"{self.__name}, {self.__observations} observations"
+    
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    station = WeatherStation("Houston")
+    station.add_observation("Rain 10mm")
+    station.add_observation("Sunny")
+    print(station.latest_observation())
+
+    station.add_observation("Thunderstorm")
+    print(station.latest_observation())
+
+    print(station.number_of_observations())
+    print(station)
