f8fa594c6148f25a2789131fc8e0cd796e030603
abushka110 <qyjodixilu@gmail.com>
2024-05-07 19:03:46 +0200

    solved - advanced - part 9 - Scope of methods

--- advancedCourseInProgramming/Part9/4-Scope-Of-Methods/1-Service-Charge.py
+++ advancedCourseInProgramming/Part9/4-Scope-Of-Methods/1-Service-Charge.py
@@ -1,5 +1,32 @@
 # solution
+class BankAccount:
+    def __init__(self, owner: str, account_number: str, balance: float):
+        self.__owner = owner
+        self.__account_number = account_number
+        self.__balance = balance
+    
+    def __service_charge(self):
+        self.__balance *= 0.99
+
+    def deposit(self, amount: float):
+        self.__balance += amount
+        self.__service_charge()
+
+    def withdraw(self, amount: float):
+        if self.__balance >= amount:
+            self.__balance -= amount
+            self.__service_charge()
+
+    # A getter method
+    @property
+    def balance(self):
+        return self.__balance
+
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    account = BankAccount("Randy Riches", "12345-6789", 1000)
+    account.withdraw(100)
+    print(account.balance)
+    account.deposit(100)
+    print(account.balance)
