af7100cc41f1e38297c7a11dc1a3e3e30fafa99a
abushka110 <qyjodixilu@gmail.com>
2024-05-07 19:17:32 +0200

    solved - advanced - part 9 - Class Attributes - 2

--- advancedCourseInProgramming/Part9/5-Class-Attributes/2-List-Helper.py
+++ advancedCourseInProgramming/Part9/5-Class-Attributes/2-List-Helper.py
@@ -1,5 +1,25 @@
 # solution
+class ListHelper:
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        most_common = None
+        most_common_count = 0
+        for item in my_list:
+            if my_list.count(item) > most_common_count:
+                most_common = item
+                most_common_count = my_list.count(item)
+        return most_common
+    
+    @classmethod
+    def doubles(cls, my_list: list):
+        doub = []
+        for item in my_list:
+            if my_list.count(item) >= 2 and item not in doub:
+                doub.append(item)
+        return len(doub)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))
