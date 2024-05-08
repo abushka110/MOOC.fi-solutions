c59f8cfe5b7eaea2cfc46436e8d1174f4173b6fd
abushka110 <qyjodixilu@gmail.com>
2024-05-08 22:30:45 +0200

    solved - advanced - part 9 - MEWC - 1 - 1

--- advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
+++ advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
@@ -1,5 +1,26 @@
 # solution
+class Item:
+    def __init__(self, item_name: str, item_weight: int):
+        self.item_name = item_name
+        self.item_weight = item_weight
+
+    def __str__(self) -> str:
+        return f"{self.item_name} ({self.item_weight} kg)"
+    
+    def name(self) -> str:
+        return self.item_name
+    
+    def weight(self) -> int:
+        return self.item_weight
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    # test 1
+    book = Item("ABC Book", 2)
+    phone = Item("Nokia 3210", 1)
+
+    print("Name of the book:", book.name())
+    print("Weight of the book:", book.weight())
+
+    print("Book:", book)
+    print("Phone:", phone)
