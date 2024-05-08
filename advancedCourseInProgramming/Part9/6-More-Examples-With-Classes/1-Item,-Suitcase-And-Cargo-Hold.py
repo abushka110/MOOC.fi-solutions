f6c8701e881c7fdbf51b5672cd3f81ac3dfadd44
abushka110 <qyjodixilu@gmail.com>
2024-05-08 22:53:09 +0200

    solved - advanced - part 9 - MEWC - 1 - 6

--- advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
+++ advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
@@ -44,6 +44,23 @@ class Suitcase:
                 heaviest = item
         return heaviest
 
+class CargoHold:
+    def __init__(self, max_weight):
+        self.maximum_weight = max_weight
+        self.__suitcases_weight = 0
+        self.__suitcases = []
+
+    def __str__(self) -> str:
+        if len(self.__suitcases) == 1:
+            return f"{len(self.__suitcases)} suitcase, space for {self.maximum_weight - self.__suitcases_weight} kg"
+        else:
+            return f"{len(self.__suitcases)} suitcases, space for {self.maximum_weight - self.__suitcases_weight} kg"
+
+    def add_suitcase(self, suitcase: Suitcase):
+        if self.__suitcases_weight + suitcase.weight() <= self.maximum_weight:
+            self.__suitcases_weight += suitcase.weight()
+            self.__suitcases.append(suitcase)
+
 # test
 if __name__ == "__main__":
     # test 1
@@ -92,14 +109,36 @@ if __name__ == "__main__":
 
 
     # test 4
+    # book = Item("ABC Book", 2)
+    # phone = Item("Nokia 3210", 1)
+    # brick = Item("Brick", 4)
+
+    # suitcase = Suitcase(10)
+    # suitcase.add_item(book)
+    # suitcase.add_item(phone)
+    # suitcase.add_item(brick)
+
+    # heaviest = suitcase.heaviest_item()
+    # print(f"The heaviest item: {heaviest}")
+
+
+    # test 5
+    cargo_hold = CargoHold(1000)
+    print(cargo_hold)
+
     book = Item("ABC Book", 2)
     phone = Item("Nokia 3210", 1)
     brick = Item("Brick", 4)
 
-    suitcase = Suitcase(10)
-    suitcase.add_item(book)
-    suitcase.add_item(phone)
-    suitcase.add_item(brick)
+    adas_suitcase = Suitcase(10)
+    adas_suitcase.add_item(book)
+    adas_suitcase.add_item(phone)
 
-    heaviest = suitcase.heaviest_item()
-    print(f"The heaviest item: {heaviest}")
+    peters_suitcase = Suitcase(10)
+    peters_suitcase.add_item(brick)
+
+    cargo_hold.add_suitcase(adas_suitcase)
+    print(cargo_hold)
+
+    cargo_hold.add_suitcase(peters_suitcase)
+    print(cargo_hold)
