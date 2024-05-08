a0829bd91440382eb36103bed0b774e670e9d830
abushka110 <qyjodixilu@gmail.com>
2024-05-08 22:55:56 +0200

    solved - advanced - part 9 - MEWC - 1 - 7

--- advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
+++ advancedCourseInProgramming/Part9/6-More-Examples-With-Classes/1-Item,-Suitcase-And-Cargo-Hold.py
@@ -61,6 +61,10 @@ class CargoHold:
             self.__suitcases_weight += suitcase.weight()
             self.__suitcases.append(suitcase)
 
+    def print_items(self):
+        for suitcase in self.__suitcases:
+            print(suitcase)
+
 # test
 if __name__ == "__main__":
     # test 1
@@ -123,9 +127,28 @@ if __name__ == "__main__":
 
 
     # test 5
-    cargo_hold = CargoHold(1000)
-    print(cargo_hold)
+    # cargo_hold = CargoHold(1000)
+    # print(cargo_hold)
 
+    # book = Item("ABC Book", 2)
+    # phone = Item("Nokia 3210", 1)
+    # brick = Item("Brick", 4)
+
+    # adas_suitcase = Suitcase(10)
+    # adas_suitcase.add_item(book)
+    # adas_suitcase.add_item(phone)
+
+    # peters_suitcase = Suitcase(10)
+    # peters_suitcase.add_item(brick)
+
+    # cargo_hold.add_suitcase(adas_suitcase)
+    # print(cargo_hold)
+
+    # cargo_hold.add_suitcase(peters_suitcase)
+    # print(cargo_hold)
+
+
+    # test 6
     book = Item("ABC Book", 2)
     phone = Item("Nokia 3210", 1)
     brick = Item("Brick", 4)
@@ -137,8 +160,9 @@ if __name__ == "__main__":
     peters_suitcase = Suitcase(10)
     peters_suitcase.add_item(brick)
 
+    cargo_hold = CargoHold(1000)
     cargo_hold.add_suitcase(adas_suitcase)
-    print(cargo_hold)
-
     cargo_hold.add_suitcase(peters_suitcase)
-    print(cargo_hold)
+
+    print("The suitcases in the cargo hold contain the following items:")
+    cargo_hold.print_items()
