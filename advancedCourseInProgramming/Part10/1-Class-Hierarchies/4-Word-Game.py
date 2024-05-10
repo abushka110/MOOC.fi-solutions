6ffa12f4b8cff2ae53dc63ab95146f7a4f918b36
abushka110 <qyjodixilu@gmail.com>
2024-05-10 10:58:32 +0200

    solved - advanced - part 10 - Class Hierarchies - 4 - 2

--- advancedCourseInProgramming/Part10/1-Class-Hierarchies/4-Word-Game.py
+++ advancedCourseInProgramming/Part10/1-Class-Hierarchies/4-Word-Game.py
@@ -43,7 +43,29 @@ class LongestWord(WordGame):
         elif len(player1_word) < len(player2_word):
             return 2
     
+class MostVowels(WordGame):
+    def __init__(self, rounds: int):
+        self.wins1 = 0
+        self.wins2 = 0
+        super().__init__(rounds)
+
+    def round_winner(self, player1_word: str, player2_word: str):
+        vowels = "aeiouy"
+        player1_vowels_count = 0
+        for letter in player1_word:
+            if letter in vowels:
+                player1_vowels_count += 1
+        
+        player2_vowels_count = 0
+        for letter in player2_word:
+            if letter in vowels:
+                player2_vowels_count += 1
+
+        if player1_vowels_count > player2_vowels_count:
+            return 1
+        elif player1_vowels_count < player2_vowels_count:
+            return 2
+            
+
 # test
-if __name__ == "__main__":
-    p = LongestWord(3)
-    p.play()
+# if __name__ == "__main__":
