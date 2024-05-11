69effb7d51598ada508e8cec61d2e25db3955579
abushka110 <qyjodixilu@gmail.com>
2024-05-11 19:48:29 +0200

    solved - advanced - part 10 - OOPt - 1 - 1

--- advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/1-Money.py
+++ advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/1-Money.py
@@ -1,4 +1,14 @@
 # solution
+class Money:
+    def __init__(self, euros: int, cents: int):
+        self.euros = euros
+        self.cents = cents
+
+    def __str__(self):
+        if self.cents < 10:
+            return f"{self.euros}.0{self.cents} eur"
+        else:
+            return f"{self.euros}.{self.cents} eur"
 
 # test
 if __name__ == "__main__":
