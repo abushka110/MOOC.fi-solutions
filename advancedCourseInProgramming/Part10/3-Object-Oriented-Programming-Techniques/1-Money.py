0aa87f19dc1fab3b8dbf33b21e6a47c2112065cf
abushka110 <qyjodixilu@gmail.com>
2024-05-11 19:52:52 +0200

    solved - advanced - part 10 - OOPt - 1 - 2

--- advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/1-Money.py
+++ advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/1-Money.py
@@ -9,7 +9,27 @@ class Money:
             return f"{self.euros}.0{self.cents} eur"
         else:
             return f"{self.euros}.{self.cents} eur"
+        
+    def __eq__(self, another):
+        if (self.euros + self.cents / 100) == (another.euros + another.cents / 100):
+            return True
+        else:
+            return False
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    # test 1
+    e1 = Money(4, 10)
+    e2 = Money(2, 5)
+    e3 = Money(4, 10)
+    print(e1)
+    print(e2)
+    print(e3)
+    print(e1 == e2)
+    print(e1 == e3)
+    # expected output:
+    # 4.10 eur
+    # 2.05 eur
+    # 4.10 eur
+    # False
+    # True
