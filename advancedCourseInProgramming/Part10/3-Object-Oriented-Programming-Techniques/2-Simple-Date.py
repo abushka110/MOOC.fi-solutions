59e24e84af59f13f3cb34233da17e0386dd99aa1
abushka110 <qyjodixilu@gmail.com>
2024-05-13 23:30:20 +0200

    solved - advanced - part 10 - OOPt - 2 - 2

--- advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
+++ advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
@@ -20,18 +20,44 @@ class SimpleDate:
     def __gt__(self, other):
         return (self.year, self.month, self.day) > (other.year, other.month, other.day)
     
+    def __add__(self, days):
+        day = self.day
+        month = self.month
+        year = self.year
+
+        day += days
+        while day > 30:
+            day -= 30
+            month += 1
+            if month > 12:
+                month = 1
+                year += 1
+
+        return SimpleDate(day, month, year)
     
+
+
 
 # test
 if __name__ == "__main__":
-    d1 = SimpleDate(4, 10, 2020)
-    d2 = SimpleDate(28, 12, 1985)
-    d3 = SimpleDate(28, 12, 1985)
+    # test 1
+    # d1 = SimpleDate(4, 10, 2020)
+    # d2 = SimpleDate(28, 12, 1985)
+    # d3 = SimpleDate(28, 12, 1985)
+    # print(d1)
+    # print(d2)
+    # print(d1 == d2)
+    # print(d1 != d2)
+    # print(d1 == d3)
+    # print(d1 < d2)
+    # print(d1 > d2)
 
-    print(d1)
-    print(d2)
-    print(d1 == d2)
-    print(d1 != d2)
-    print(d1 == d3)
-    print(d1 < d2)
-    print(d1 > d2)
+    # test 2
+    # d1 = SimpleDate(4, 10, 2020)
+    # d2 = SimpleDate(28, 12, 1985)
+    # d3 = d1 + 3
+    # d4 = d2 + 400
+    # print(d1)
+    # print(d2)
+    # print(d3)
+    # print(d4)
