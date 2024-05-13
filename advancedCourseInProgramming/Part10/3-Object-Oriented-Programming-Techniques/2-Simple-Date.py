d0c36c7cc06d3243ff99efc0042d5fe77f1fd1d1
abushka110 <qyjodixilu@gmail.com>
2024-05-13 23:38:01 +0200

    solved - advanced - part 10 - OOPt - 2 - with model solution

--- advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
+++ advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
@@ -43,6 +43,47 @@ class SimpleDate:
         total_days_other = other.day + other.month * 30 + other.year * 360
 
         return abs(total_days_self - total_days_other)  
+    
+# model solution
+# class SimpleDate:
+#     def __init__(self, pv: int, month: int, year: int):
+#         self.__pv = pv
+#         self.__month = month
+#         self.__year = year
+ 
+#     def __str__(self):
+#         return f'{self.__pv}.{self.__month}.{self.__year}'
+ 
+#     # Comparisons are easier, when date is converted to days
+#     def __value(self):
+#         return self.__year * 360 + self.__month * 30 + self.__pv
+ 
+#     # Converst days back to date
+#     def __to_date(self, days: int):
+#         months = days // 30
+#         years = months // 12
+#         days -= months * 30
+#         months -= years * 12
+#         return SimpleDate(days, months, years)
+ 
+#     def __lt__(self, other: "SimpleDate"):
+#         return self.__value() < other.__value()
+ 
+#     def __gt__(self, other: "SimpleDate"):
+#         return self.__value() > other.__value()
+ 
+#     def __eq__(self, other: "SimpleDate"):
+#         return self.__value() == other.__value()
+        
+#     def __ne__(self, other: "SimpleDate"):
+#         return self.__value() != other.__value()
+ 
+#     def __add__(self, days_to_add: int):
+#         return self.__to_date(self.__value() + days_to_add)
+ 
+#     def __sub__(self, other: "SimpleDate"):
+#         # abs(x) returns the absolute value of x
+#         return abs(self.__value() - other.__value())
 
 # test
 if __name__ == "__main__":
