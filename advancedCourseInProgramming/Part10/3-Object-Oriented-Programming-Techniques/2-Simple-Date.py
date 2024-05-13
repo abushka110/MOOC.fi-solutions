0be3f52672655f5ecfb278b6bfb0c5e28b89f21b
abushka110 <qyjodixilu@gmail.com>
2024-05-13 23:25:10 +0200

    solved - advanced - part 10 - OOPt - 2 - 1

--- advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
+++ advancedCourseInProgramming/Part10/3-Object-Oriented-Programming-Techniques/2-Simple-Date.py
@@ -1,5 +1,37 @@
 # solution
+class SimpleDate:
+    def __init__(self, day, month, year) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __str__(self):
+        return f"{self.day}.{self.month}.{self.year}"
+
+    def __eq__(self, other):
+        return (self.year, self.month, self.day) == (other.year, other.month, other.day)
+
+    def __ne__(self, other):
+        return (self.year, self.month, self.day) != (other.year, other.month, other.day)
+
+    def __lt__(self, other):
+        return (self.year, self.month, self.day) < (other.year, other.month, other.day)
+
+    def __gt__(self, other):
+        return (self.year, self.month, self.day) > (other.year, other.month, other.day)
+    
+    
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(28, 12, 1985)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d1)
+    print(d2)
+    print(d1 == d2)
+    print(d1 != d2)
+    print(d1 == d3)
+    print(d1 < d2)
+    print(d1 > d2)
