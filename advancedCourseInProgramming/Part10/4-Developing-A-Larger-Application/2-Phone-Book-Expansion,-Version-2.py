9ff01d3e6c3d0a8e04356373f4f717c471711529
abushka110 <qyjodixilu@gmail.com>
2024-05-15 15:33:31 +0200

    solved - advanced - part 10 - DevALargerApps - 2 - 1

--- advancedCourseInProgramming/Part10/4-Developing-A-Larger-Application/2-Phone-Book-Expansion,-Version-2.py
+++ advancedCourseInProgramming/Part10/4-Developing-A-Larger-Application/2-Phone-Book-Expansion,-Version-2.py
@@ -1,5 +1,77 @@
-# solution
+# Write your solution here:
+class Person:
+    def __init__(self, name) -> None:
+        self.__person_name = name
+        self.__person_numbers = []
+        self.__person_address = None
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+    def name(self):
+        return self.__person_name
+    
+    def numbers(self):
+        return self.__person_numbers
+    
+    def address(self):
+        return self.__person_address
+    
+    def add_number(self, number_to_add):
+        self.__person_numbers.append(number_to_add)
+
+    def add_address(self, address_to_add):
+        self.__person_address = address_to_add
+
+
+class PhoneBook:
+    def __init__(self):
+        self.__persons = {}
+
+    def get_entry(self, name: str):
+        if not name in self.__persons:
+            return None
+        return self.__persons[name]
+
+    def all_entries(self):
+        return self.__persons
+
+class PhoneBookApplication:
+    def __init__(self):
+        self.__phonebook = PhoneBook()
+
+    def help(self):
+        print("commands: ")
+        print("0 exit")
+        print("1 add number")
+        print("2 search")
+
+    def add_number(self):
+        name = input("name: ")
+        number = input("number: ")
+        self.__phonebook.add_number(name, number)
+
+    def search(self):
+        name = input("name: ")
+        numbers = self.__phonebook.get_entry(name)
+        if numbers == None:
+            print("number unknown") 
+            return 
+        for number in numbers:
+            print(number)       
+
+    def execute(self):
+        self.help()
+        while True:
+            print("")
+            command = input("command: ")
+            if command == "0":
+                break
+            elif command == "1":
+                self.add_number()
+            elif command == "2":
+                self.search()
+            else:
+                self.help()
+
+
+# when testing, no code should be outside application except the following:
+application = PhoneBookApplication()
+application.execute()
