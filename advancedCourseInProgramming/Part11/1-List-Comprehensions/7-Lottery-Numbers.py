f93946e8487ae4981a9c577d038570e240e1b04a
abushka110 <qyjodixilu@gmail.com>
2024-05-24 12:22:10 +0200

    solved - Part 11 - List Comprehensions - 7 - 1

--- advancedCourseInProgramming/Part11/1-List-Comprehensions/7-Lottery-Numbers.py
+++ advancedCourseInProgramming/Part11/1-List-Comprehensions/7-Lottery-Numbers.py
@@ -1,5 +1,20 @@
 # solution
+class LotteryNumbers:
+    def __init__(self, week: int, seven_integers: list):
+        self.week = week
+        self.seven_integers = seven_integers
+
+    def number_of_hits(self, numbers: list) -> list:
+        return len([number for number in numbers if number in self.seven_integers])
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    # test 1
+    week5 = LotteryNumbers(5, [1,2,3,4,5,6,7])
+    my_numbers = [1,4,7,11,13,19,24]
+    print(week5.number_of_hits(my_numbers))
+
+    # test 2
+    # week8 = LotteryNumbers(8, [1,2,3,10,20,30,33])
+    # my_numbers = [1,4,7,10,11,20,30]
+    # print(week8.hits_in_place(my_numbers))
