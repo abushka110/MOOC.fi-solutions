30dec7b37a1a4083545c9a2228fe00a26c05451a
abushka110 <qyjodixilu@gmail.com>
2024-05-24 11:53:01 +0200

    solved - Part 11 - List Comprehensions - 2

--- advancedCourseInProgramming/Part11/1-List-Comprehensions/2-Rows-Of-Stars.py
+++ advancedCourseInProgramming/Part11/1-List-Comprehensions/2-Rows-Of-Stars.py
@@ -1,5 +1,15 @@
 # solution
+def rows_of_stars(numbers: list):
+    return ["*" * number for number in numbers]
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    rows = rows_of_stars([1,2,3,4])
+    for row in rows:
+        print(row)
+
+    print()
+
+    rows = rows_of_stars([4, 3, 2, 1, 10])
+    for row in rows:
+        print(row)
