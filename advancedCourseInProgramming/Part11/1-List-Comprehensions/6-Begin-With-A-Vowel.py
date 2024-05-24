1d0db47b207b5131de1244028d7f7ce24821acd1
abushka110 <qyjodixilu@gmail.com>
2024-05-24 12:07:27 +0200

    solved - Part 11 - List Comprehensions - 6

--- advancedCourseInProgramming/Part11/1-List-Comprehensions/6-Begin-With-A-Vowel.py
+++ advancedCourseInProgramming/Part11/1-List-Comprehensions/6-Begin-With-A-Vowel.py
@@ -1,5 +1,9 @@
 # solution
+def begin_with_vowel(words: list) -> list:
+    return [word for word in words if word[0].upper() in "AEIOU"]
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    word_list = ["automobile","motorbike","Animal","cat","Dog","APPLE","orange"]
+    for vowelled in begin_with_vowel(word_list):
+        print(vowelled)
