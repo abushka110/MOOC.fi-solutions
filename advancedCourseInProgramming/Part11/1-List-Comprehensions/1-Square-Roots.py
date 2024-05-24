f928a4dd38ea5f4c0b66e86d20a6a227af748a99
abushka110 <qyjodixilu@gmail.com>
2024-05-24 11:47:05 +0200

    solved - Part 11 - List Comprehensions - 1

--- advancedCourseInProgramming/Part11/1-List-Comprehensions/1-Square-Roots.py
+++ advancedCourseInProgramming/Part11/1-List-Comprehensions/1-Square-Roots.py
@@ -1,5 +1,16 @@
 # solution
+from math import sqrt
+
+def square_roots(origin_list: list) -> list:
+    return [sqrt(item) for item in origin_list]
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    lines = square_roots([1,2,3,4])
+    for line in lines:
+        print(line)
+    # expected output:
+    # 1.0
+    # 1.4142135623730951
+    # 1.7320508075688772
+    # 2.0
