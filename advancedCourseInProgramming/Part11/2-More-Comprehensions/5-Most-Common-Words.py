153324140be96bcf4bc073ae54fcb5996a3f5ee7
abushka110 <qyjodixilu@gmail.com>
2024-05-27 16:09:42 +0200

    solved - Part 11 - More Comprehensions - 5

--- advancedCourseInProgramming/Part11/2-More-Comprehensions/5-Most-Common-Words.py
+++ advancedCourseInProgramming/Part11/2-More-Comprehensions/5-Most-Common-Words.py
@@ -1,5 +1,19 @@
 # solution
+from string import punctuation
+
+def most_common_words(filename: str, lower_limit: int):
+    text = ""
+    with open(filename) as file:
+        text = file.read()
+        # remove punctuation marks from strings
+        for punctuation_mark in punctuation:
+            text = text.replace(punctuation_mark, "")
+    
+    # split text into strings in list
+    text_strings = text.split()
+
+    return {word : text_strings.count(word) for word in text_strings if text_strings.count(word) >= lower_limit}
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    print(most_common_words("src/comprehensions.txt", 3))
