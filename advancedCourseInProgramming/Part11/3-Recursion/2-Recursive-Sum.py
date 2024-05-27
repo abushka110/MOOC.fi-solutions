45e5f0a6b14a7a593c6c6999e9e3c8a92d998005
abushka110 <qyjodixilu@gmail.com>
2024-05-27 22:50:37 +0200

    solved - Part 11 - More Recursion - 2

--- advancedCourseInProgramming/Part11/3-Recursion/2-Recursive-Sum.py
+++ advancedCourseInProgramming/Part11/3-Recursion/2-Recursive-Sum.py
@@ -1,5 +1,15 @@
 # solution
+def recursive_sum(number: int):
+    # if the number is 1, there is nothing else to add
+    if number <= 1:
+        return number
+    
+    # rest of the function
+    return number + recursive_sum(number - 1)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    result = recursive_sum(3)
+    print(result)
+    print(recursive_sum(5))
+    print(recursive_sum(10))
