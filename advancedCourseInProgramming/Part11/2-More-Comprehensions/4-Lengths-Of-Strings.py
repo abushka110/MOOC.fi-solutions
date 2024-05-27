649964424557b2afa3d940821a1f990508bf1441
abushka110 <qyjodixilu@gmail.com>
2024-05-27 12:58:12 +0200

    solved - Part 11 - More Comprehensions - 4

--- advancedCourseInProgramming/Part11/2-More-Comprehensions/4-Lengths-Of-Strings.py
+++ advancedCourseInProgramming/Part11/2-More-Comprehensions/4-Lengths-Of-Strings.py
@@ -1,5 +1,9 @@
 # solution
+def lengths(strings: list):
+    return {string : len(string) for string in strings}
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    word_list = ["once", "upon" , "a", "time", "in"]
+    word_lengths = lengths(word_list)
+    print(word_lengths)
