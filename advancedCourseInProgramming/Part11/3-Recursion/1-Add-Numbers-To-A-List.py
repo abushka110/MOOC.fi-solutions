1a7d30224686a8a75f72c20122671eb4c4da3e6c
abushka110 <qyjodixilu@gmail.com>
2024-05-27 21:57:42 +0200

    solved - Part 11 - More Recursion - 1

--- advancedCourseInProgramming/Part11/3-Recursion/1-Add-Numbers-To-A-List.py
+++ advancedCourseInProgramming/Part11/3-Recursion/1-Add-Numbers-To-A-List.py
@@ -1,5 +1,11 @@
 # solution
-
+def add_numbers_to_list(numbers: list):
+    while len(numbers) % 5 != 0:
+        numbers.append(numbers[len(numbers) - 1] + 1)
+        add_numbers_to_list(numbers)
+        
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    numbers = [1,3,4,5,10,11]
+    add_numbers_to_list(numbers)
+    print(numbers)
