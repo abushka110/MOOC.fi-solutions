4f10793657e3cc2385de22f1109559d16c17d867
abushka110 <qyjodixilu@gmail.com>
2024-06-12 17:27:21 +0200

    better solution

--- advancedCourseInProgramming/Part12/1-Functions-As-Arguments/2-Sort-By-Number-Of-Seasons.py
+++ advancedCourseInProgramming/Part12/1-Functions-As-Arguments/2-Sort-By-Number-Of-Seasons.py
@@ -3,8 +3,7 @@ def sort_by_seasons(items: list):
     def number_of_seasons(show: dict):
         return show["seasons"]
     
-    sorted_shows = sorted(items, key=number_of_seasons)
-    return sorted_shows
+    return sorted(items, key=number_of_seasons)
 
 # test
 if __name__ == "__main__":
