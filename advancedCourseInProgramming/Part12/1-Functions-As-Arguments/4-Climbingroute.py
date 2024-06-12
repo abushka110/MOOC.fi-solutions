c826a14ebaeaaa79d4f559dc481865fd6841b090
abushka110 <qyjodixilu@gmail.com>
2024-06-12 17:55:43 +0200

    solved - Part 12 - FAA - 4

--- advancedCourseInProgramming/Part12/1-Functions-As-Arguments/4-Climbingroute.py
+++ advancedCourseInProgramming/Part12/1-Functions-As-Arguments/4-Climbingroute.py
@@ -13,15 +13,28 @@ def sort_by_length(routes: list):
         return route.length
     return list(reversed(sorted(routes, key=get_length)))
 
+def sort_by_difficulty(routes: list):
+    def route_difficulty(route):
+        return (route.grade, route.length)
+    return sorted(routes, key=route_difficulty, reverse=True)
+
 # test
 if __name__ == "__main__":
     # test 1
+    # r1 = ClimbingRoute("Edge", 38, "6A+")
+    # r2 = ClimbingRoute("Smooth operator", 11, "7A")
+    # r3 = ClimbingRoute("Synchro", 14, "8C+")
+    # r4 = ClimbingRoute("Small steps", 12, "6A+")
+    # routes = [r1, r2, r3, r4]
+    # for route in sort_by_length(routes):
+    #     print(route)
+    
+    # test 2
     r1 = ClimbingRoute("Edge", 38, "6A+")
     r2 = ClimbingRoute("Smooth operator", 11, "7A")
     r3 = ClimbingRoute("Synchro", 14, "8C+")
     r4 = ClimbingRoute("Small steps", 12, "6A+")
+
     routes = [r1, r2, r3, r4]
-    for route in sort_by_length(routes):
+    for route in sort_by_difficulty(routes):
         print(route)
-    
-    # test 2
