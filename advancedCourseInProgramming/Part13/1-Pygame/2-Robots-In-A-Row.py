f468919f3f86bf483663ff4c5622a3b103a32c20
abushka110 <qyjodixilu@gmail.com>
2024-06-13 21:47:15 +0200

    solved - Part 13 - Pygame - 2

--- advancedCourseInProgramming/Part13/1-Pygame/2-Robots-In-A-Row.py
+++ advancedCourseInProgramming/Part13/1-Pygame/2-Robots-In-A-Row.py
@@ -1,5 +1,22 @@
-# solution
+import pygame
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0, 0, 0))
+
+width = robot.get_width()
+height = robot.get_height()
+
+for i in range(10):
+    window.blit(robot, (50+50*i, 100))
+
+pygame.display.flip()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
