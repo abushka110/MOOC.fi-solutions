99659a6d1d4c1ea5691342a0ad9d29053ef97141
abushka110 <qyjodixilu@gmail.com>
2024-06-13 21:39:19 +0200

    solved - Part 13 - Pygame - 1

--- advancedCourseInProgramming/Part13/1-Pygame/1-Four-Robots.py
+++ advancedCourseInProgramming/Part13/1-Pygame/1-Four-Robots.py
@@ -1,5 +1,23 @@
-# solution
+import pygame
 
-# test
-if __name__ == "__main__":
-    pass  # Add your test code here
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0, 0, 0))
+
+width = robot.get_width()
+height = robot.get_height()
+window.blit(robot, (0, 0))
+window.blit(robot, (640-width, 0))
+window.blit(robot, (0, 480-height))
+window.blit(robot, (640-width, 480-height))
+
+pygame.display.flip()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
