316d975e4978e24c4d6bb89bdc84ba36356bbff7
abushka110 <qyjodixilu@gmail.com>
2024-07-12 23:23:16 +0200

    solved - Part 12 - FAA - 7

--- advancedCourseInProgramming/Part12/1-Functions-As-Arguments/7-Product-Search.py
+++ advancedCourseInProgramming/Part12/1-Functions-As-Arguments/7-Product-Search.py
@@ -1,5 +1,32 @@
 # solution
+def search(products: list, criterion: callable):
+    # create new list
+    new_list = []
+
+    # check if product fulfil the criterion
+    for product in products:
+        if criterion(product):
+            new_list.append(product)
+    
+    return new_list
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22), ("kale", 0.99, 1)]
+
+    def price_under_4_euros(product):
+        return product[1] < 4
+    
+    for product in search(products, price_under_4_euros):
+        print(product)
+    
+    # expected output:
+    # ('apple', 3.95, 3)
+    # ('kale', 0.99, 1)
+
+    for product in search(products, lambda t: t[2]>10):
+        print(product)
+
+    # expected output:
+    # ('banana', 5.95, 12)
+    # ('watermelon', 4.95, 22)
