0e2afb0811515c5337124cb8997743bc62c7163a
abushka110 <qyjodixilu@gmail.com>
2024-07-13 15:16:45 +0200

    solved - Part 12 - Generators - 1

--- advancedCourseInProgramming/Part12/2-Generators/1-Even-Numbers.py
+++ advancedCourseInProgramming/Part12/2-Generators/1-Even-Numbers.py
@@ -1,5 +1,33 @@
 # solution
+def even_numbers(beginning: int, maximum: int):
+    if beginning % 2 != 0:
+        beginning += 1
+    while beginning <= maximum:
+        yield beginning
+        beginning += 2
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    # test 1
+    numbers = even_numbers(2, 10)
+    for number in numbers:
+        print(number)
+    # expected output:
+    # 2
+    # 4
+    # 6
+    # 8
+    # 10
+
+    print()
+
+    # test 2
+    numbers = even_numbers(11, 21)
+    for number in numbers:
+        print(number)
+    # expected output:
+    # 12
+    # 14
+    # 16
+    # 18
+    # 20
