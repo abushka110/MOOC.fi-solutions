4b6da481d6241b1d1dc1a5cd52d26ccec2cec06c
abushka110 <qyjodixilu@gmail.com>
2024-07-13 23:11:30 +0200

    solved - Part 12 - Generators - 2

--- advancedCourseInProgramming/Part12/2-Generators/2-Prime-Numbers.py
+++ advancedCourseInProgramming/Part12/2-Generators/2-Prime-Numbers.py
@@ -1,5 +1,31 @@
 # solution
+def prime_numbers():
+    number = 2
+
+    while True:
+        if is_prime(number):
+            yield number
+        number += 1
+
+# function that checks if the number is prime or not
+def is_prime(number: int):
+        for i in range(2, number-1):
+            if number % i == 0:
+                return False
+        
+        return True
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
+    # expected output:
+    # 2
+    # 3
+    # 5
+    # 7
+    # 11
+    # 13
+    # 17
+    # 19
