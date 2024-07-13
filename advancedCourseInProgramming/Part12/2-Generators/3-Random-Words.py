2dcae9899ea45a782ddd1ca4dffd881431088589
abushka110 <qyjodixilu@gmail.com>
2024-07-13 23:28:55 +0200

    solved - Part 12 - Generators - 3

--- advancedCourseInProgramming/Part12/2-Generators/3-Random-Words.py
+++ advancedCourseInProgramming/Part12/2-Generators/3-Random-Words.py
@@ -1,5 +1,17 @@
 # solution
+import random
+
+def word_generator(characters: str, length: int, amount: int):
+    return(''.join(random.sample(characters, length)) for i in range(amount))
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    word_gen = word_generator("abcdefg", 3, 5)
+    for word in word_gen:
+        print(word)
+    # expected output:
+    # dbf
+    # baf
+    # ead
+    # fga
+    # ccc
