73fd8333babd153206c9dc255a937f05582225d6
abushka110 <qyjodixilu@gmail.com>
2024-07-15 15:52:03 +0200

    solved - Part 12 - Functional Programming - 3

--- advancedCourseInProgramming/Part12/3-Functional-Programming/3-Study-Credits.py
+++ advancedCourseInProgramming/Part12/3-Functional-Programming/3-Study-Credits.py
@@ -1,5 +1,55 @@
+from functools import reduce
+
+class CourseAttempt:
+    def __init__(self, course_name: str, grade: int, credits: int):
+        self.course_name = course_name
+        self.grade = grade
+        self.credits = credits
+
+    def __str__(self):
+        return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
+
 # solution
+def sum_of_all_credits(attempts: list):
+    return reduce(lambda sum, attempt: sum + attempt.credits, attempts, 0)
+
+def sum_of_passed_credits(attempts: list):
+    passed_attempts = list(filter(lambda attempt: attempt.grade > 0, attempts))
+    return reduce(lambda sum, attempt: sum + attempt.credits, passed_attempts, 0)
+
+def average(attempts: list):
+    passed_attempts = list(filter(lambda attempt: attempt.grade > 0, attempts))
+    return reduce(lambda sum, attempt: sum + attempt.grade, passed_attempts, 0) / len(passed_attempts)
 
 # test
 if __name__ == "__main__":
-    pass  # Add your test code here
+    print("TEST 1")
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
+    # expected output:
+    # 20
+
+    print()
+
+    print("TEST 2")
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_passed_credits([s1, s2, s3])
+    print(credit_sum)
+    # expected output:
+    # 15
+
+    print()
+
+    print("TEST 3")
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    ag = average([s1, s2, s3])
+    print(ag)
+    # expected output:
+    # 4.0
